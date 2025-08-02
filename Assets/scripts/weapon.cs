30154dccec1e30f57329881343ae56da3e231d3e
Rt9i <158214881+Rt9i@users.noreply.github.com>
2025-08-02 12:04:55 +0300

    .

--- Assets/scripts/weapon.cs
+++ Assets/scripts/weapon.cs
@@ -2,42 +2,38 @@ using UnityEngine;
 
 public class weapon : MonoBehaviour
 {
+
     [SerializeField] PlayerInputs playerInputs;
     [SerializeField] Camera cam;
     [SerializeField] AudioSource gunShotSFX;
-    [SerializeField] private Animator animator;
-    [SerializeField] float fireDelay = 0.25f;
+    bool shoot;
+    float damage = 10;
     RaycastHit hit;
-    float damage = 10f;
-    float nextFireTime = 0f;
-
     void Update()
     {
         if (playerInputs.shoot)
         {
             shoot = playerInputs.shoot;
-            
+            gunShotSFX.Play();
             Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity);
 
 
             if (hit.collider)
             {
                 Debug.Log("hit a: " + hit.collider.name);
-                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                // if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+                // {
+                //     hit.collider.gameObject.GetComponent<robot>().enabled = false;
+                // }
+
+                shoot = playerInputs.shoot;
+
+                Debug.Log("hit a: " + hit.collider.name);
+                if (hit.collider.gameObject.CompareTag("Robot"))
                 {
-                    hit.collider.gameObject.GetComponent<robot>().enabled = false;
+                    hit.collider.gameObject.GetComponent<health>().Damage(damage);
                 }
-
-            shoot = playerInputs.shoot;
-
-            Debug.Log("hit a: " + hit.collider.name);
-            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
-            {
-                hit.collider.gameObject.GetComponent<health>().Damage(damage);
             }
-
-
-
 
         }
     }
